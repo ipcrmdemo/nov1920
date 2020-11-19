ec790ae466c59bbcd0bf17ddf15a7341251b91d4
Matt Cadorette <mcadorette@gmail.com>
2020-11-19 09:28:30 -0500

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -24,4 +24,4 @@ app.get(["/", "/:name"], (req, res) => {
 });
 
 // tslint:disable-next-line:no-console
-ap.listen(8080, () => console.log("App listening on port 8080!"))
+app.listen(8080, () => console.log("App listening on port 8080!"))
